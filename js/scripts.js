33c87a104e5e7587705d23d9a1c07e4116240f78
Ben White <bwhite2140@outlook.com>
2020-06-04 11:30:38 -0700

    add javascript brances
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-  
+  $("#form").submit(function(event) {
+    event.preventDefault();
+    const height = parseInt($("input#height").val());
+    if (height <= 122) {
+      $(".hidden#kid").show();
+      $(".hidden#classic").hide();
+      $(".hidden#fast").hide();
+    } else if (height >= 168) {
+      $(".hidden#kid").hide();
+      $(".hidden#classic").show();
+      $(".hidden#fast").show();
+    } else {
+      $(".hidden#kid").show();
+      $(".hidden#classic").show();
+      $(".hidden#fast").hide();
+    }
+  });
 });
